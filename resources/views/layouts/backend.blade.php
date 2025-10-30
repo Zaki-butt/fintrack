e8f0d886a468cf7ce759b9919809fe0ac2358787
Zakria-Butt23 <zakria@wistech.biz>
2025-10-30 13:46:10 +0500

    fixed

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Finance Tracker - Dashboard</title>
     @vite('resources/css/app.css')
 </head>
+
 <body class="bg-gray-100 text-gray-900">
 
     {{-- ✅ Sidebar (fixed left side) --}}
@@ -14,12 +16,11 @@
     {{-- ✅ Navbar (fixed top) --}}
     @include('layouts.navbar')
 
-   <main class="pt-20 ml-64 px-12 pb-12">
-    @yield('content')
-</main>
-
-
+    <main class="pt-20 ml-64 px-12 pb-12">
+        @yield('content')
+    </main>
 
     @vite('resources/js/app.js')
 </body>
+
 </html>
